ab26d571d7e56fc06fb6cf38e4595fca82cb673c
Hampus Fridholm <hampus.fridholm@gmail.com>
2024-09-22 14:55:07 +0200

    Initialized a new minesweeper repo and copied over all assets from the previous minesweeper repo. Added debug.c and a makefile.

---
+++ source/minesweeper.c
@@ -0,0 +1,19 @@
+/*
+ * minesweeper
+ *
+ * Written by Hampus Fridholm
+ *
+ * Last updated: 2024-09-22
+ */
+
+#include "minesweeper.h"
+
+int main(int argc, char* argv[])
+{
+  info_print("Start of main");
+
+
+  info_print("End of main");
+
+  return 0;
+}
---
+++ source/minesweeper.h
@@ -0,0 +1,14 @@
+/*
+ * minesweeper.h
+ *
+ * Written by Hampus Fridholm
+ *
+ * Last updated: 2024-09-22
+ */
+
+#ifndef MINESWEEPER_H
+#define MINESWEEPER_H
+
+#include "debug.h"
+
+#endif // MINESWEEPER_H
